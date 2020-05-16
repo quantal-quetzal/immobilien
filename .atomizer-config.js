0878cf475b6d4049670db844082ed9c081b4f5d5
Felix Gehring <github@felix-gehring.de>
2020-05-16 20:14:11 +0200

    Atomizer eingebaut

---
+++ .atomizer-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    breakPoints: {
+        sm: '@media (min-width: 768px)',
+        md: '@media (min-width: 992px)',
+        lg: '@media (min-width: 1200px)',
+    },
+    custom: {},
+    classNames: [],
+};
---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+const Atomizer = require('atomizer');
+const atomizerConfig = require('./.atomizer-config.js');
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addTransform('atomicCss', function (content, outputPath) {
+        const atomizer = new Atomizer({ verbose: true });
+        // Parse text to find Atomic CSS classes
+        const foundClasses = atomizer.findClassNames(content);
+        const finalConfig = atomizer.getConfig(foundClasses, atomizerConfig);
+        const css = atomizer.getCss(finalConfig);
+
+        return content.replace('</body>', `<style>\n${css}</style><body>`);
+    });
+
+    return {
+        dir: {
+            input: 'src',
+        },
+    };
+};
